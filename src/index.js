f77d9d9e84400092c73eb5327323dbf2339617a2
niko <niko@istackmanila.com>
2018-06-28 16:57:36 +0800

    Added not found page.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import configureStore from './app/store';
 import routes from './app/routes';
 import rootSaga from './app/sagas';
+import NotFound from './app/common/containers/not-found';
 import './styles/css/index.css';
 
 const store = configureStore();
@@ -14,9 +15,10 @@ store.runSaga(rootSaga);
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <div>
+      <Switch>
         {routes.map((route, i) => <Route key={`route-${i}`} {...route} />)}
-      </div>
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
